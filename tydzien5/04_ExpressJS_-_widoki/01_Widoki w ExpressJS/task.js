a3dca10108ba56839cb29b414edc31548c0aeec6
Pietrzi <pietrzi@op.pl>
2020-05-14 19:09:27 +0200

    W50401

--- tydzien5/04_ExpressJS_-_widoki/01_Widoki w ExpressJS/task.js
+++ tydzien5/04_ExpressJS_-_widoki/01_Widoki w ExpressJS/task.js
@@ -4,9 +4,16 @@ import { runAssertions } from './internals/assertions';
 
 const app = express();
 const port = 3000;
+app.set('view engine', 'ejs');
+app.set('views', 'widoki');
+
 
 try {
   // Add your config here
+
+  app.get('/hello', (req, res) => {
+    res.render('hello', { greet: req.query.greet} )
+  })
 
   app.listen(port, async () => {
     console.log(`Example app listening at http://localhost:${port}`);
@@ -23,3 +30,26 @@ try {
 }
 
 
+// import express from 'express';
+
+// import { runAssertions } from './internals/assertions';
+
+// const app = express();
+// const port = 3000;
+
+// try {
+//   // Add your config here
+
+//   app.listen(port, async () => {
+//     console.log(`Example app listening at http://localhost:${port}`);
+
+//     console.log('Will run assertions...');
+//     try {
+//       await runAssertions();
+//     } catch (err) {
+//       console.log('Error when trying to run assertions: ', err);
+//     }
+//   });
+// } catch (err) {
+//   console.error('App loading error: ', err);
+// }

897a6e70a6f128fafe2c9ad0c33079aa47c52791
Pietrzi <pietrzi@op.pl>
2020-05-14 19:41:51 +0200

    W50402

--- tydzien5/04_ExpressJS_-_widoki/02_Silniki - porównanie/task.js
+++ tydzien5/04_ExpressJS_-_widoki/02_Silniki - porównanie/task.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import fs from 'fs';
+import haml from 'hamljs';
 
 import { runAssertions } from './internals/assertions';
 import initEngines from './internals/initEngines';
 
 const app = express();
 const port = 3000;
+
+// app.set('view engine', 'ejs');
+// app.set('view engine', 'pug');
+// app.set('view engine', 'handlebars');
+// app.set('view engine', 'haml');
 
 try {
   initEngines(app);
@@ -16,6 +23,23 @@ try {
   };
 
   // Add your routes with rendering here
+
+  app.get('/pug', (req, res) => {
+    res.render('comparison.pug', data);
+  });
+
+  app.get('/hbs', (req, res) => {
+    res.render('comparison.handlebars', data);
+  });
+
+  app.get('/haml', (req, res) => {
+    const hamlView = fs.readFileSync('views/comparison.haml', 'utf8');
+    res.end( haml.render(hamlView, {locals: data}) );
+  });
+
+  app.get('/ejs', (req, res) => {
+    res.render('comparison.ejs', data);
+  });
 
   app.listen(port, async () => {
     console.log(`Example app listening at http://localhost:${port}`);

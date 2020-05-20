c9b2752cc488bd279e7988552e335c8970153ac0
Pietrzi <pietrzi@op.pl>
2020-05-20 10:15:05 +0200

    6W0202

--- tydzien6/02_Zaawansowany_Socketio_statyczny_hosting_oraz_alternatywy/02_Socket.io - pokoje i broadcast/client.js
+++ tydzien6/02_Zaawansowany_Socketio_statyczny_hosting_oraz_alternatywy/02_Socket.io - pokoje i broadcast/client.js
@@ -2,3 +2,27 @@ import io from 'socket.io-client';
 import { generate } from 'randomstring';
 
 // Add your code BELOW
+// const socket = io({query: {clientId}});
+// let clientId = generate();
+
+// const form = document.querySelector("request-sender");
+
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
+//     let value = document.getElementById('name').value
+//     socket.emit('joinRoom', )
+// })
+
+let clientId = generate()
+
+
+const socket = io({query: {clientId}})
+const form = document.getElementById('request-sender')
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  let value = document.getElementById('name').value
+  socket.emit('joinRoom', value)
+})
+socket.on('users', users => {
+  console.log(users)
+})
--- tydzien6/02_Zaawansowany_Socketio_statyczny_hosting_oraz_alternatywy/02_Socket.io - pokoje i broadcast/task.js
+++ tydzien6/02_Zaawansowany_Socketio_statyczny_hosting_oraz_alternatywy/02_Socket.io - pokoje i broadcast/task.js
@@ -1,6 +1,6 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import express from 'express';
+import express from 'express'; ///, { response } 
 import exhbs from 'express-handlebars';
 import { Server } from 'http';
 import io from 'socket.io';
@@ -31,6 +31,41 @@ try {
   app.use(express.static('public'));
 
   // Implement your view rendering AND socket handling BELOW
+  app.get('/', (req, res) => {
+    res.render('room')
+  })
+
+  // server.on('connection', socket => {
+  //   let user = {id: socket.handshake.query.clientId}
+
+  //   socket.on('joinRoom', (name) => {
+  //     if (!guests[user.id]) {
+
+  //     user.name = name
+
+  //     guests[user.id] = user
+  //     }
+
+  //   })
+
+  //   console.log(guests)
+  // })
+
+  server.on('connection', socket => {
+    let user = {id: socket.handshake.query.clientId}
+    socket.on('joinRoom', name => {
+      if (!guests[user.id]) {
+        user.name = name
+        guests[user.id] = user
+        socket.join('meeting')
+        server.to('meeting').emit('users', guests)
+      }
+    })
+    socket.on('disconnect', () => {
+      delete guests[user.id]
+      socket.emit('users', guests)
+    })
+  })
 
   http.listen(port, async () => {
     console.log(`Socket.io server listening at http://localhost:${port}`);

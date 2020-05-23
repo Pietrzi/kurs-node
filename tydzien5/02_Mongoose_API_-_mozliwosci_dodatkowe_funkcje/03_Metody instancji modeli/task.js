3a6804f2f77c9692289fe85a9be500ee33bdd217
Pietrzi <pietrzi@op.pl>
2020-05-23 14:06:27 +0200

    W50203

--- tydzien5/02_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/03_Metody instancji modeli/task.js
+++ tydzien5/02_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/03_Metody instancji modeli/task.js
@@ -9,7 +9,53 @@ import { runAssertions } from './internals/assertions';
 
     let Order;
 
-    // Add all of your code below
+    // Add all of your code below ///////////////////////////////
+
+    const productSchema = new mongoose.Schema({
+      name: String,
+      brand: String,
+      available: Number,
+      lastOrderDate: Date,
+      unitPrice: Number,
+      supplierName: String,
+      expirationDate: Date,
+      categories: {
+        type: [String],
+        enum: ['coffee', 'food', 'accessories', 'equipment', 'premium']
+      }
+    });
+    
+    const Product = mongoose.model('Product', productSchema);
+
+    const orderSchema = new mongoose.Schema({
+      date: Date,
+      location: String,
+      paidIn: {
+        type: String,
+        enum: ['cash', 'card']
+      },
+      staffId: mongoose.Schema.ObjectId,
+      products: Array,
+      total: {
+        type: Number,
+        min: 1
+      }
+    });
+    
+    orderSchema.methods.getRelatedProducts = function() {
+      const productIds = (
+        this.products && this.products.map(product => product.productId)
+      ) || [];
+
+      const foundProducts = Product.find({ _id: { $in: productIds }});
+    
+      return foundProducts;
+    };
+    
+    Order = mongoose.model('Order', orderSchema);
+
+
+    ///////////////////////////////////////////////////////////////////
 
     await runAssertions(Order);
   } catch (err) {

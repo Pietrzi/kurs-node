5e81ff0e6bcf416f01e4e9d554db24a260835090
Pietrzi <pietrzi@op.pl>
2020-05-23 13:34:14 +0200

    W50205

--- tydzien5/02_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/05_Hooki/task.js
+++ tydzien5/02_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/05_Hooki/task.js
@@ -10,7 +10,71 @@ import { runAssertions } from './internals/assertions';
     let Comment;
     let Movie;
 
-    // Add all of your code below
+    // Add all of your code below ///////////////////////////////////////////
+
+    const commentSchema = new mongoose.Schema({
+      text: {
+        type: String,
+        required: true
+      },
+      author: {
+        type: String,
+        required: true
+      },
+      likes: {
+        type: Number,
+        required: true
+      }
+    });
+
+    Comment = mongoose.model('Comment', commentSchema);
+
+    const movieSchema = new mongoose.Schema({
+      movieID: {
+        type: Number,
+        required: true,
+      },
+      intRating: {
+        type: Number,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      timestamp:{
+        type: Number,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      platform: {
+        type: String,
+        enum: ['Netflix', 'HBO Go', 'Prime'],
+        required: true,
+      },
+      date: {
+        type: Date,
+        required: true,
+      },
+      comments: {
+        type: [String],
+        required: true,
+      },
+    });
+    
+    movieSchema.post('remove',function() {
+      const comments = this.comments || [];
+      if (comments && comments.length) {
+        return Comment.remove({ _id: { $in: comments } }).exec();
+      }
+    });
+    
+    Movie = mongoose.model('Movie', movieSchema);
+
+    ////////////////////////////////////
 
     await runAssertions(Comment, Movie);
   } catch (err) {

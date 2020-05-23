983e09a5e35539ed5020ed7de54fe79af26558ef
Pietrzi <pietrzi@op.pl>
2020-05-23 14:13:37 +0200

    W50202

--- tydzien5/02_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/02_Właściwości wirtualne/task.js
+++ tydzien5/02_Mongoose_API_-_mozliwosci_dodatkowe_funkcje/02_Właściwości wirtualne/task.js
@@ -10,7 +10,59 @@ import { runAssertions } from './internals/assertions';
     let books; // find() result should be assigned to this property
     let Book; // Book model should be assigned to this property
 
-    // Add all of your code below
+    // Add all of your code below //////////////////////////////////
+
+    const booksSchema = new mongoose.Schema({
+      author: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      language: {
+        type: String,
+        required: true,
+      },
+      pages: {
+        type: Number,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      year: {
+        type: Number,
+        required: true
+      },
+      amount: {
+        type: Number,
+        required: true
+      },
+      price: {
+        type: Number,
+        required: true
+      }
+    });
+
+    booksSchema.virtual('totalStoreValue').get(function() {
+      return this.amount * this.price;
+    });
+
+    booksSchema.virtual('bookDetails').set(function(value) {
+      const splitParts = value.split(' - ');
+      this.author = splitParts[0];
+      this.title = splitParts[1];
+    }).get(function() {
+      return `${this.author} - ${this.title}`
+    });
+
+
+    Book = new mongoose.model('Book', booksSchema);
+
+    ////////////////////////////////////////////////////////////////
 
     await runAssertions(Book, books);
   } catch (err) {

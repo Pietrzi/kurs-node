62db997a5bd52a27f0d96fe41cb1ab98d1baf2da
Pietrzi <pietrzi@op.pl>
2020-05-25 23:48:41 +0200

    W50107

--- tydzien5/01_Mongoose_ODM_-_przekrój_działania/07_Dokumenty - instancje modelu/task.js
+++ tydzien5/01_Mongoose_ODM_-_przekrój_działania/07_Dokumenty - instancje modelu/task.js
@@ -11,6 +11,59 @@ import { runAssertions } from './internals/assertions';
 
     // Add all of your code below
 
+    const ingredientSchema = new mongoose.Schema({
+      Name: {
+        type: String,
+        required: true
+      },
+      Calories: {
+        type: Number,
+        required: true
+      },
+      CookingTime: {
+        type: Number,
+        required: true
+      },
+      Tags: [new mongoose.Schema({
+        Tag: String
+      })],
+      Portions: new mongoose.Schema({
+        SMALL: {
+          type: Number,
+          required: true,
+          min: 1
+        },
+        MEDIUM: {
+          type: Number,
+          required: true,
+          min: 1
+        },
+        LARGE: {
+          type: Number,
+          required: true,
+          min: 1
+        },
+      }),
+      FlavorValues: {
+        type: new mongoose.Schema({
+          SOUR: Number,
+          SALT: Number,
+          ACID: Number,
+          SWEET: Number,
+          FAT: Number,
+          UMAMI: Number
+        }),
+        default: {
+          SOUR: 0,
+          SALT: 0,
+          ACID: 0,
+          SWEET: 0,
+          FAT: 0,
+          UMAMI: 0
+        }
+      }
+    });
+
     await runAssertions(tomato);
   } catch (err) {
     console.log('Error when running the task: ', err);

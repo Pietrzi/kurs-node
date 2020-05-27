93be6b74356272d4b8380c48f18967cd7dcfe8f1
Pietrzi <pietrzi@op.pl>
2020-05-27 22:37:37 +0200

    W50104

--- tydzien5/01_Mongoose_ODM_-_przekrój_działania/04_Walidacja i funkcje/task.js
+++ tydzien5/01_Mongoose_ODM_-_przekrój_działania/04_Walidacja i funkcje/task.js
@@ -6,7 +6,52 @@ import { runAssertions } from './internals/assertions';
   try {
     let gameSchema;
 
-    // Put your schema here
+    // Put your schema here ///////////////////////
+
+    gameSchema = new mongoose.Schema({
+      title: {
+        required: true,
+        type: String
+      },
+      publisher: {
+        required: true,
+        type: String
+      },
+      releaseDate: {
+        required: true,
+        type: Date
+      },
+      meanRating: {
+        type: Number,
+        min: [1, 'Mean rating should be between 1 and 10, is: {VALUE}'],
+        max: [10, 'Mean rating should be between 1 and 10, is: {VALUE}'],
+      },
+      tags: {
+        type: [String],
+        enum: [`action`, `rpg`, `mmo`, `fps`],
+        validate: [
+          function validateTags(val) {
+            return val ? val.length > 0 : true;
+          },
+          'Tags should be represented by at least one tag'
+        ]
+      },
+      retailPrice: {
+        type: Number,
+        min: [0, 'Retail price should be bigger or equal to 0, is: {VALUE}'],
+      },
+      comments: {
+        type: [String],
+        validate: [
+          function validateTags(val) {
+            return val ? val.length > 0 : true;
+          },
+          'Comments cannot be empty'
+        ]
+      },
+    });
+
+    ///////////////////////////////////////////////
 
     await runAssertions(gameSchema);
   } catch (err) {

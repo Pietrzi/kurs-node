2d715b7c1587122c115212ab505716cb754d65cc
Pietrzi <pietrzi@op.pl>
2020-05-29 21:23:05 +0200

    W50104

--- tydzien5/01_Mongoose_ODM_-_przekrój_działania/05_Gettery i settery/task.js
+++ tydzien5/01_Mongoose_ODM_-_przekrój_działania/05_Gettery i settery/task.js
@@ -6,7 +6,36 @@ import { runAssertions } from './internals/assertions';
   try {
     let stockTransactionSchema;
 
-    // Put your schema here
+    // Put your schema here /////////////////////
+
+    const roundToTwoDecimals = val => Math.round(val * 100) / 100;
+
+stockTransactionSchema = new mongoose.Schema({
+  price: {
+    type: Number,
+    required: true,
+    get: (val) => roundToTwoDecimals(val),
+    set: (val) => roundToTwoDecimals(val)
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  market: {
+    type: String,
+    required: true
+  },
+  symbol: {
+    type: String,
+    required: true
+  },
+  industry: {
+    type: String,
+    required: true
+  }
+});
+
+    ////////////////////////////////////////////
 
     await runAssertions(stockTransactionSchema);
   } catch (err) {

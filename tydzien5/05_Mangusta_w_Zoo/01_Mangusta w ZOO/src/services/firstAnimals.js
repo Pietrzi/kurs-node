7e6229246d025ca31b9ce4b19b966f215ade03ed
Pietrzi <pietrzi@op.pl>
2020-06-02 13:17:58 +0200

    W50501 service/animals

--- tydzien5/05_Mangusta_w_Zoo/01_Mangusta w ZOO/src/services/firstAnimals.js
+++ tydzien5/05_Mangusta_w_Zoo/01_Mangusta w ZOO/src/services/firstAnimals.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 import { MISSING_DATA, NOT_FOUND, VALIDATION_ERROR } from '../constants/error';
-import { addAnimal, deleteAnimal, getAnimals } from '../models/animals';
 
 export default class Animals {
   async addAnimal(animalData) {
@@ -9,17 +8,7 @@ export default class Animals {
       throw new Error(MISSING_DATA);
     }
 
-    try {
-      return await addAnimal(animalData);
-    } catch (err) {
-      if (err.name === 'ValidationError') {
-        const error = new Error(VALIDATION_ERROR);
-        error.reason = err.message;
-        throw error;
-      }
-
-      throw err;
-    }
+    return '123123123123123123';
   }
 
   async deleteAnimal(animalId) {
@@ -31,15 +20,13 @@ export default class Animals {
       throw error;
     }
 
-    const deletedCount = await deleteAnimal(animalId);
-    if (deletedCount === 0) {
-      throw new Error(NOT_FOUND);
-    }
-
     return true;
   }
 
   async getAnimals(page = 0) {
-    return getAnimals(page);
+    return {
+      pages: 1,
+      animals: [],
+    };
   }
 }

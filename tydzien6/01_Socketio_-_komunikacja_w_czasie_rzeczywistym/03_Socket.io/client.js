53a123825e867248be7b7d6a372d49b3414a3d71
Pietrzi <pietrzi@op.pl>
2020-06-03 11:02:37 +0200

    W60103

--- tydzien6/01_Socketio_-_komunikacja_w_czasie_rzeczywistym/03_Socket.io/client.js
+++ tydzien6/01_Socketio_-_komunikacja_w_czasie_rzeczywistym/03_Socket.io/client.js
@@ -3,9 +3,24 @@ import throttle from 'lodash-es/throttle';
 
 const socket = io();
 
+
+///////////////////////////
+socket.on('annoy', (data) => {
+    document.getElementById('annoy-container').innerHTML = data;
+  });
+  
+  socket.on('clear', (data) => {
+    document.getElementById('annoy-container').innerHTML = '';
+  });
+  
+  const reactToMouseMove = throttle((event) => {
+    socket.emit('activity', { pageX: event.pageX, pageY: event.pageY });
+  }, 1000);
+///////////////////////////
+
 // Add your listeners HERE
 
 // Add your throttled! method HERE
 
 // MOdify this line to use throttled method
-window.addEventListener('mousemove', () => {});
+window.addEventListener('mousemove', reactToMouseMove);
--- tydzien6/01_Socketio_-_komunikacja_w_czasie_rzeczywistym/03_Socket.io/task.js
+++ tydzien6/01_Socketio_-_komunikacja_w_czasie_rzeczywistym/03_Socket.io/task.js
@@ -11,18 +11,53 @@ const port = 3000;
 const http = Server(app);
 const server = io(http);
 
+
+
 const clearAnnoyTimers = (prevTimers) => {
-  // Clear timeouts here using `clearTimeout` with each of passed timers
+  // Clear timeouts here using `clearTimeout` with each of passed timers //////
+  for (const timer of prevTimers) {
+    clearTimeout(timer);
+  }
+  ///////////////////////////////
 };
 
 const setAnnoyTimers = (connected, prevTimers) => {
   // Add timer creation here (and cleaning the old ones)
   // Return array of new timers
-  // Remember to call "emit('annoy')" properly for each timeout!
+  // Remember to call "emit('annoy')" properly for each timeout! ////////////
+  clearAnnoyTimers(prevTimers);
+
+  connected.emit('clear');
+
+  const newTimers = [];
+
+  newTimers.push(setTimeout(() => {
+    connected.emit('annoy', `<img src="/doggo.jpg" width="200px;"><h4>Please move your mouse...</h4>`);
+  }, 5000));
+  newTimers.push(setTimeout(() => {
+    connected.emit('annoy', '<img src="/raccoon.jpg" width="200px"><h3>Would you please move your mouse?</h3>');
+  }, 10000));
+  newTimers.push(setTimeout(() => {
+    connected.emit('annoy', '<img src="/cat.jpg" width="200px"><h2>MOVE MOVE MOVE!</h2>');
+  }, 20000));
+
+  return newTimers;
+  ///////////////////////////////////////////////////////////////////////////
 };
 
 const annoy = (socket) => {
-  // Implement timers array management here and reacting to `active` and `disconnect` events here
+  // Implement timers array management here and reacting to `active` and `disconnect` events here ////
+  let timers = [];
+
+  socket.on('activity', (data) => {
+    console.log('New activity!', data);
+    timers = setAnnoyTimers(socket, timers);
+  });
+
+  socket.on('disconnect', () => {
+    clearAnnoyTimers(timers);
+  });
+  ///////////////////////
 };
 
 try {
@@ -41,7 +76,16 @@ try {
 
   app.use(express.static('public'));
 
-  // Add endpoints and socket listener (.on('connection') here
+  // Add endpoints and socket listener (.on('connection') here //////////////////
+  app.get('/', (req, res) => {
+    return res.render('activityTracker');
+  });
+
+  // Add Socket.io communication
+  server.on('connection', (socket) => {
+    annoy(socket);
+  });
+  /////////////////////////////////////////////////////////////////////////////////
 
   http.listen(port, async() => {
     console.log(`Socket.io server listening at http://localhost:${port}`);
